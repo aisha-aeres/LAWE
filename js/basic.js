52e97dfd0f2aae4436a0bdf38ced465c6d42656a
Aïsha <3025058@aeres.nl>
2019-04-11 16:32:48 +0200

    last
    last

--- js/basic.js
+++ js/basic.js
@@ -2,3 +2,84 @@ function tothetop () {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+//initialize the map
+
+  var map = L.map('map').setView([45.438296, 12.333483], 13);
+
+  //Create baselayer - tiles
+  var backgroundMap = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 18,
+	ext: 'png'
+});
+backgroundMap.addTo(map);
+
+var waveicon = L.icon({
+    iconUrl: 'img/icon1.png',
+
+    iconSize: [38, 30],
+    iconArchor: [22, 94],
+    popupArchor: [-3, -76]
+});
+
+var marker = L.marker([45.438369, 12.318234], {icon: waveicon}).addTo(map);
+
+// Create a marker first
+var geojsonMarkerOptions = {
+  radius: 8,
+  fillColor: "#ff7800",
+  color: "#000",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8
+};
+
+//create a empty geojson layer
+var elbelayer = L.geoJson(null,{
+  pointToLayer: function (feature, latlng) {
+    return L.circleMarker(latlng, geojsonMarkerOptions);
+  }
+}).addTo(map);
+
+ // new Http Request
+var elbe = new XMLHttpRequest();
+// set the request method and data file
+elbe.open('GET', encodeURI("js/mapelbe.geojson"));
+//specify what must be done with the geojson data to the layer when request is succesfull
+elbe.onload = function() {
+
+  if (elbe.readyState === 4) {
+      // add the json data to the geojson layer we created before!
+      elbelayer.addData(JSON.parse(elbe.responseText));
+    } else {
+      alert('Request failed.  Returned status of ' + elbe.status);
+    }
+};
+
+elbe.send();
+
+//create a empty geojson layer
+var elbelayer = L.geoJson(null,{
+  pointToLayer: function (feature, latlng) {
+    return L.circleMarker(latlng, geojsonMarkerOptions);
+  }
+}).addTo(map);
+ // new Http Request
+var elbe = new XMLHttpRequest();
+// set the request method and data file
+elbe.open('GET', encodeURI("js/mapelbe.geojson"));
+//specify what must be done with the geojson data to the layer when request is succesfull
+elbe.onload = function() {
+  if (elbe.readyState === 4) {
+      // add the json data to the geojson layer we created before!
+      elbelayer.addData(JSON.parse(elbe.responseText));
+    } else {
+      alert('Request failed.  Returned status of ' + elbe.status);
+    }
+};
+// send the request
+elbe.send();
--- js/leaflet.js
+++ js/leaflet.js
@@ -1,21 +1,44 @@
-var map = L.geoJson(null,{
-  pointToLayer: function (feature, latlng) {
-    return L.circleMarkers (latlng, geojsonMarkerOptions2);
+//initialize the map
+
+  var map = L.map('map').setView([45.438296, 12.333483], 13);
+
+  //Create baselayer - tiles
+  var backgroundMap = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 18,
+	ext: 'png'
+});
+
+//Create a marker first
+var geojsonMarkerOptions = {
+  radius: 8,
+  fillColor: "#ff7800",
+  color: "#000",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8
+};
+
+//create a empty geojson layer
+var geojson = L.geojson(null,{
+  pointToLayer: function (feature, latlng){
+    return L.cirleMarker(latlng, geojsonMarkerOptions);
   }
 }).addTo(map);
 
-var xhttp2 = new XMLHttpRequest();
-
-xhttp2.open ('GET',
-encodeURI ("mapelbe.geojson"));
-
-xhttp2.onload = function () {
-  if (xhttp2.readyState === 4) {
-
-      map.addData(JSON.parse(xhttp2.responseText));
-  } else {
-    alert('Request Failed. Return status of' + xhttp2.status);
-  }
+// new Http Request
+var xhttp = new XMLHttpRequest();
+// set the request method and data file
+xhttp.open('GET', encodeURI("mapelbe.geojson"));
+//specify what must be done with the geojson data to the layer when request is succesfull
+xhttp.onload = function() {
+if (xhttp.readyState === 4) {
+// add the json data to the geojson layer we created before!
+geojson.addData(JSON.parse(xhttp.responseText));
+} else {
+alert('Request failed. Returned status of ' + xhttp.status);
+}
 };
-
-xhttp2.send();
+// send the req
